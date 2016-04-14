80d3ca8e5d90f20d379dae6a34bb140af5fc0de8
MartinBialkowski <rejestracjamb@gmail.com>
2016-04-14 19:24:10 +0200

    Added files via upload

---
+++ IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Insert(T element);
+        void Delete(int id);
+        void Update(T element);
+        void Save();
+    }
+}
